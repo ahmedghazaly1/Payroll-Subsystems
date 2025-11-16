256358c2653f4a8a07820c615a20f0381332cc0f
khaledgiu <khaled.abdelrehim@student.giu-uni.de>
2025-11-16 21:57:29 +0200

    Remove node_modules from repo

--- dist/src/integration/time-management/time-management.interface.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=time-management.interface.js.map
--- dist/src/modules/tax-rule/tax-rule.service.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=tax-rule.service.js.map
