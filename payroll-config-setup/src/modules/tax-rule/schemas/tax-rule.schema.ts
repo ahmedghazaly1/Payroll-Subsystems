c35cc1b0881962f6765a36aeaa22ad988b56435a
rawdatarekkkk <rawdatarek5588@gmail.com>
2025-11-19 12:49:00 +0200

    Update tax-rule.schema.ts

--- payroll-config-setup/src/modules/tax-rule/schemas/tax-rule.schema.ts
+++ payroll-config-setup/src/modules/tax-rule/schemas/tax-rule.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import * as mongoose from 'mongoose';
-import { AllowanceDocument } from '../../allowance/schemas/allowance.schema';
+import { AllowanceDocument } from '../schemas/allowance.schema';
 
 export type TaxRuleDocument = TaxRule & Document;
 
